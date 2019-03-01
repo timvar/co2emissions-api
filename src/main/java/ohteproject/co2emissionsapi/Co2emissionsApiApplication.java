524f87090ddc95ff718a11f4e416aef40248019b
Timo Varila <timo.varila@gmail.com>
2019-03-01 11:52:30 +0200

    1st

---
+++ src/main/java/ohteproject/co2emissionsapi/Co2emissionsApiApplication.java
@@ -0,0 +1,13 @@
+package ohteproject.co2emissionsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Co2emissionsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Co2emissionsApiApplication.class, args);
+	}
+
+}
